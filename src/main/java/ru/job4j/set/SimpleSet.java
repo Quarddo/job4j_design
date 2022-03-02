96b10e5f54902b966b185ece1f0fab2689a67a80
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-03-03 00:34:46 +0500

    1. Реализовать коллекцию Set на массиве [#996 #271099]

--- src/main/java/ru/job4j/set/SimpleSet.java
+++ src/main/java/ru/job4j/set/SimpleSet.java
@@ -16,10 +16,9 @@ public class SimpleSet<T> implements Set<T> {
      */
     @Override
     public boolean add(T value) {
-        boolean rsl = false;
-        if (!contains(value)) {
+        boolean rsl = !contains(value);
+        if (rsl) {
             set.add(value);
-            rsl = true;
         }
         return rsl;
     }
