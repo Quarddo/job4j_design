d8db0b02e6554256f49a300acc0dac2b12cf4514
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-03-02 20:29:26 +0500

    1. Реализовать коллекцию Set на массиве [#996]

---
+++ src/main/java/ru/job4j/set/SimpleSet.java
@@ -0,0 +1,52 @@
+package ru.job4j.set;
+
+import ru.job4j.list.SimpleArrayList;
+
+import java.util.Iterator;
+import java.util.Objects;
+
+public class SimpleSet<T> implements Set<T> {
+
+    private SimpleArrayList<T> set = new SimpleArrayList<>(10);
+
+    /**
+     * Метод добавляет новое уникальное значение в коллекцию
+     * @param value добавляемое значение
+     * @return true если добавляемый в коллекцию элемент уникальный,
+     */
+    @Override
+    public boolean add(T value) {
+        boolean rsl = false;
+        if (!contains(value)) {
+            set.add(value);
+            rsl = true;
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод сравнивает содержит ли коллекция данный елемент
+     * если данный елемент присутствует в коллекции, метод возвращает true
+     * @param value елемент,по которому осуществляется сравнение с коллекцией
+     * @return true если елемент value есть в коллекции
+     */
+    @Override
+    public boolean contains(T value) {
+        boolean rsl = false;
+        for (T obj : set) {
+            if (Objects.equals(value, obj)) {
+                rsl = true;
+            }
+        }
+        return rsl;
+    }
+
+    /**
+     * Метод осуществляет перебор коллекции
+     * @return список елементов присутствующих в коллекции
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return set.iterator();
+    }
+}
