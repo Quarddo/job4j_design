38abfa5380140ee5763fa7d318a968489f3ff941
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-03-03 15:45:35 +0500

    2. Без переопределения equals и hashCode [#1005]

--- src/main/java/ru/job4j/map/User.java
+++ src/main/java/ru/job4j/map/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.map;
 
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Модель User
@@ -17,5 +20,52 @@ public class User {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "name='" + name + '\''
+                + ", children=" + children
+                + ", birthday=" + birthday
+                + '}';
+    }
+
+    public static void main(String[] args) {
+        User us1 = new User("Max", 1, new GregorianCalendar(1995, Calendar.MAY, 21));
+        User us2 = new User("Max", 1, new GregorianCalendar(1995, Calendar.MAY, 21));
+
+        Map<User, Object> map = new HashMap<>(
+                Map.of(
+                        us1, new Object(),
+                        us2, new Object())
+        );
+        System.out.println(map);
+        System.out.println(us1.hashCode());
+        System.out.println(us2.hashCode());
     }
 }
