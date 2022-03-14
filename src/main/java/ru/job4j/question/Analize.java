a97de86c7f1c7b372d432a61265235a81326469a
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-03-14 20:04:26 +0500

    2. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/question/Analize.java
@@ -0,0 +1,28 @@
+package ru.job4j.question;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+
+public class Analize  {
+
+    public static Info diff(Set<User> previous, Set<User> current) {
+        Info info = new Info(0,0,0);
+        Map<Integer, String> newMap = current.stream()
+                .collect(Collectors.toMap(User::getId, User::getName));
+        for (User user : previous) {
+                String name = newMap.get(user.getId());
+            if (!newMap.containsKey(user.getId())) {
+                info.added++;
+            }
+            if (name != null && !name.equals(user.getName())) {
+                info.changed++;
+            }
+            if (name == null) {
+                info.delete++;
+            }
+        }
+        return new Info(info.added, info.changed, info.delete);
+    }
+}
---
+++ src/main/java/ru/job4j/question/Info.java
@@ -0,0 +1,59 @@
+package ru.job4j.question;
+
+import java.util.Objects;
+
+public class Info {
+
+    int added;
+    int changed;
+    int delete;
+
+    public Info(int added, int changed, int delete) {
+        this.added = added;
+        this.changed = changed;
+        this.delete = delete;
+    }
+
+    public int getAdded() {
+        return added;
+    }
+
+    public void setAdded(int added) {
+        this.added = added;
+    }
+
+    public int getChanged() {
+        return changed;
+    }
+
+    public void setChanged(int changed) {
+        this.changed = changed;
+    }
+
+    public int getDelete() {
+        return delete;
+    }
+
+    public void setDelete(int delete) {
+        this.delete = delete;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || obj.getClass() != getClass()) {
+            return false;
+        }
+        Info info = (Info) obj;
+        return info.added == added
+                && info.changed == changed
+                && info.delete == delete;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(added, changed, delete);
+    }
+}
