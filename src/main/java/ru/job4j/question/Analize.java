734c1b029f6249ce4d8e2e0683f01e2357251bfa
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-03-14 20:06:53 +0500

    2. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class Analize  {
 
     public static Info diff(Set<User> previous, Set<User> current) {
-        Info info = new Info(0,0,0);
+        Info info = new Info(0, 0, 0);
         Map<Integer, String> newMap = current.stream()
                 .collect(Collectors.toMap(User::getId, User::getName));
         for (User user : previous) {
