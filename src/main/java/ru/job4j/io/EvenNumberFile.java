a47e00c59c82f5c928f2b5c86fa550a2c4dcd785
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-03-16 17:09:08 +0500

    0.2. FileInputStream [#4898] comment

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -3,6 +3,9 @@ package ru.job4j.io;
 import java.io.FileInputStream;
 
 public class EvenNumberFile {
+    /**
+     * Даный метод читает файл
+     */
     public static void main(String[] args) {
         try (FileInputStream in = new FileInputStream("even.txt")) {
             StringBuilder text = new StringBuilder();
@@ -10,6 +13,11 @@ public class EvenNumberFile {
             while ((read = in.read()) != -1) {
                 text.append((char) read);
             }
+            /**
+             * Данная часть кода устанавливает условия вывода текста
+             * каждый элемент начинается с новый строки
+             * И проверяет четно чилсо или нет
+             */
             String[] lines = text.toString().split(System.lineSeparator());
             for (String line : lines) {
                 String num = Integer.parseInt(line) % 2 == 0
