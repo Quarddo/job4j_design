48b8f84313f1d4d0b14283b574a44bd55d72d332
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-03-20 18:37:37 +0500

    2. Анализ доступности сервера. [#859]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -1,32 +1,31 @@
 package ru.job4j.io;
 
-import ru.job4j.collection.List;
 import java.io.*;
-import java.util.ArrayList;
 
 public class Analizy {
     public void unavailable(String source, String target) {
-        boolean serverActivity = false;
-        try (BufferedReader reader = new BufferedReader(new FileReader(source));
-             PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
-            String line = reader.readLine();
-            while (line != null && line.contains("400") || line.contains("500")) {
-                String[] rsl = line.split(" ", 2);
-                serverActivity = true;
-                writer.print("Сервер не работал с " + rsl[1]);
-                if (line.contains("200") || line.contains("300")) {
-                    serverActivity = false;
-                    writer.print("по " + rsl[1]);
-
+            boolean serverActivity = false;
+            try (BufferedReader reader = new BufferedReader(new FileReader(source));
+                 PrintWriter writer = new PrintWriter(new FileOutputStream(target))) {
+                String line = reader.readLine();
+                while (line != null) {
+                    String[] rsl = line.split(" ", 2);
+                    if (!serverActivity && (rsl[0].equals("400") || rsl[0].equals("500"))) {
+                        writer.print("Сервер не работал с " + rsl[1]);
+                        serverActivity = true;
+                    }
+                    if (serverActivity && (rsl[0].equals("200") || rsl[0].equals("300"))) {
+                        writer.print("по " + rsl[1]);
+                        serverActivity = false;
+                    }
                 }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
-    }
-
 
     public static void main(String[] args) {
-        new Analizy().unavailable("server.log", ".server/unavailable.txt");
+        Analizy analysis = new Analizy();
+        analysis.unavailable(".server/server.txt", ".server/unavailable.txt");
     }
 }
