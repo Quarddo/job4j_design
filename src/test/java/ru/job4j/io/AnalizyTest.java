7072f754bb48ebc49d2459a3401293b166b83484
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-03-21 22:50:07 +0500

    2. Анализ доступности сервера. [#859 #277644]

--- src/test/java/ru/job4j/io/AnalizyTest.java
+++ src/test/java/ru/job4j/io/AnalizyTest.java
@@ -31,7 +31,7 @@ public class AnalizyTest {
         try (BufferedReader in = new BufferedReader(new FileReader(target))) {
             in.lines().forEach(rsl::append);
         }
-        assertThat(rsl.toString(), is( "10:58:01;11:02:02"));
+        assertThat(rsl.toString(), is("10:58:01;11:02:02"));
     }
 
 }
