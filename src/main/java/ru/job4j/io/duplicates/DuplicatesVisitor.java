75504c0756ff7e9d276e4aed0373f2c403c3f2b7
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-03-22 22:48:30 +0500

    4.2. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,25 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    Set<FileProperty> notDuplicate = new HashSet<>();
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fileProper = new FileProperty(attrs.size(), file.toFile().getName());
+        if (notDuplicate.contains(fileProper)) {
+            System.out.println("Дубликат: " + file.toAbsolutePath());
+        } else {
+            notDuplicate.add(fileProper);
+        }
+        return CONTINUE;
+    }
+}
