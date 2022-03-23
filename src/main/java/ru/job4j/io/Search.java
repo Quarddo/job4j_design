521c732078a1ff11a2c42017e33508a33602b3aa
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-03-23 21:05:28 +0500

    5. Валидация параметров запуска. [#246865 #279203]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -12,7 +12,7 @@ public class Search {
     public static void main(String[] args) throws IOException {
         Path start = Paths.get(args[0]);
         if (args.length != 2) {
-            throw new IllegalArgumentException("Root folder is null. Usage java -jar dir.jar ROOT_FOLDER.");
+            throw new IllegalArgumentException("Check the number of entered arguments (there should be two)");
         }
         if (!start.toFile().exists() || !start.toFile().isDirectory()) {
             throw new IllegalArgumentException("Directory does not exist or is not a directory.");
