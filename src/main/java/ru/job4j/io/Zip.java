a924d7597a6214c0e19d10cb0096346c4c6de3c5
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-03-25 23:03:37 +0500

    5.2. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -2,26 +2,23 @@ package ru.job4j.io;
 
 import java.io.*;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
-import java.util.zip.InflaterInputStream;
 import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
 
     public void packFiles(List<Path> sources, File target) {
-       try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-           for (Path source : sources) {
-               zip.putNextEntry(new ZipEntry(source.toString()));
-               try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(source.toString()))) {
-                   zip.write(in.readAllBytes());
-               }
-           }
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
+        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
+            for (Path source : sources) {
+                zip.putNextEntry(new ZipEntry(source.toString()));
+                try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(source.toString()))) {
+                    zip.write(in.readAllBytes());
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void packSingleFile(File source, File target) {
@@ -37,21 +34,30 @@ public class Zip {
 
     public static void main(String[] args) throws IOException {
         ArgsName argsName = ArgsName.of(args);
+        Path directory = Path.of(argsName.get("d"));
+        String excludeFiles = argsName.get("e");
+        if (validate(args, directory, excludeFiles)) {
+            List<Path> filter = Search.search(directory, p -> !p.toFile().getName()
+                    .endsWith(excludeFiles));
+            File outPut = new File(argsName.get("o"));
+            Zip zip = new Zip();
+            zip.packFiles(filter, outPut);
+        }
+    }
+
+    public static boolean validate(String[] args, Path directory, String excludeFiles) {
+        ArgsName argsName = ArgsName.of(args);
         if (args.length != 3) {
             throw new IllegalArgumentException("Check the number of entered arguments (there should be three)");
         }
-        Path directory = Path.of(argsName.get("d"));
+        directory = Path.of(argsName.get("d"));
         if (!directory.toFile().exists() || !directory.toFile().isDirectory()) {
             throw new IllegalArgumentException("Directory does not exist or is not a directory.");
         }
-        String excludeFiles = argsName.get("e");
+        excludeFiles = argsName.get("e");
         if (!excludeFiles.startsWith(".")) {
             throw new IllegalArgumentException("The parameter key does not match the format.");
         }
-        List<Path> filter = Search.search(directory, p -> !p.toFile().getName()
-                .endsWith(excludeFiles));
-        File outPut = new File(argsName.get("o"));
-        Zip zip = new Zip();
-        zip.packFiles(filter, outPut);
+        return true;
     }
 }
