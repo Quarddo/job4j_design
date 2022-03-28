53d34c7de4f0e8ac5956dfada067901e02bc1ce4
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-03-28 16:16:08 +0500

    6. Кодировка. [#862]

---
+++ src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,73 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class ConsoleChat {
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+    private final String path;
+    private final String botAnswers;
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+        List<String> log = new ArrayList<>();
+        List<String> listAnswers = readPhrases();
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            String line = reader.readLine();
+            log.add(line);
+            boolean botAction = true;
+            while (!OUT.equals(line)) {
+                if (STOP.equals(line)) {
+                    botAction = false;
+                }
+                if (CONTINUE.equals(line)) {
+                    botAction = true;
+                }
+                if (botAction) {
+                    String answer = listAnswers.get(new Random().nextInt(listAnswers.size()));
+                    System.out.println(answer);
+                    log.add(answer);
+                }
+                line = reader.readLine();
+                log.add(line);
+            }
+            saveLog(log);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private List<String> readPhrases() {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(botAnswers))) {
+            list = reader.lines().collect(Collectors.toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    private void saveLog(List<String> log) {
+        try (PrintWriter writer = new PrintWriter(new FileWriter(path, true))) {
+            log.forEach(writer::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("/Users/maksimteriushov/projects/job4j_design/log.txt",
+                "/Users/maksimteriushov/projects/job4j_design/botAnswers.txt");
+        cc.run();
+    }
+}
