8e6059efa4761a96e547803f55d842e52e99bc34
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-04-06 20:30:28 +0500

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,24 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Maksim Teriushov";
+        short age = 26;
+        double weight = 75.5;
+        float height = 181.1f;
+        byte child = 1;
+        boolean haveCar = true;
+        int priceCar = 50000;
+        char currency = '$';
+        LOG.debug("User info : name - {}, age - {}, weight - {}, height - {}, child - {}, have car - {}, price car - {}, currency - {}",
+                name, age, weight, height, child, haveCar, priceCar, currency);
+
+
+
+
+        //LOG.trace("trace message");
+        //LOG.debug("debug message");
+        //LOG.info("info message");
+        //LOG.warn("warn message");
+        //LOG.error("error message");
     }
 }
