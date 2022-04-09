f18c406d81d7519a0ffb573d0c6a353059181f0c
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-04-09 18:04:02 +0500

    2. Поиск файлов по критерию [#783]

---
+++ src/main/java/ru/job4j/io/exam/ArgsName.java
@@ -0,0 +1,55 @@
+package ru.job4j.io.exam;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public int size() {
+        return values.size();
+    }
+
+    public String get(String key) {
+        if (!values.containsKey(key)) {
+            throw new IllegalArgumentException("Failed to get parameters, given key does not exist.");
+        }
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Value not found.");
+        }
+        for (String arg : args) {
+            String[] rsl = arg.split("=", 2);
+            if (checkPattern(rsl)) {
+                values.put(rsl[0].substring(1), rsl[1]);
+            }
+        }
+    }
+
+    private static boolean checkPattern(String[] rsl) {
+        if (rsl.length != 2 || rsl[0].isEmpty()
+                || !rsl[0].startsWith("-")
+                || rsl[1].isEmpty()) {
+            throw new IllegalArgumentException("Invalid value format.");
+        }
+        return true;
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ru.job4j.io.ArgsName jvm = ru.job4j.io.ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ru.job4j.io.ArgsName zip = ru.job4j.io.ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
---
+++ src/main/java/ru/job4j/io/exam/SearchByCriteria.java
@@ -0,0 +1,55 @@
+package ru.job4j.io.exam;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchByCriteria {
+
+    public static void validate(ArgsName argsName) {
+        Path path = Paths.get(argsName.get("d"));
+        if (!Files.exists(path) || !Files.isDirectory(path)) {
+            throw new IllegalArgumentException("The argument does not exist or is not a directory");
+        }
+    }
+
+    public static void handle(ArgsName argsName) throws IOException {
+        validate(argsName);
+        Path startPath = Paths.get(argsName.get("d"));
+        String typeSearch = argsName.get("t");
+        String name = argsName.get("n");
+        String out = argsName.get("o");
+        List<Path> pathList = new ArrayList<>();
+        if ("mask".contains(typeSearch)) {
+            pathList = search(startPath, path -> path.toFile().getName()
+                    .endsWith(name.replace("*", "")));
+        }
+        if ("name".contains(typeSearch)) {
+            pathList = search(startPath, path -> path.toFile().getName()
+                    .equals(name));
+        }
+        try (PrintWriter writer = new PrintWriter(new FileWriter(out))) {
+            pathList.forEach(writer::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
+        SearchFiles searcher = new SearchFiles(condition);
+        Files.walkFileTree(root, searcher);
+        return searcher.getPaths();
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length != 4) {
+            throw new IllegalArgumentException("Wrong number of arguments.");
+        }
+        ArgsName argsName = ArgsName.of(args);
+        handle(argsName);
+    }
+}
