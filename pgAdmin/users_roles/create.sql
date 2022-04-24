01e72cedcc932a6a889ab9188c644de074448b80
Quarddo <93788758+Quarddo@users.noreply.github.com>
2022-04-24 14:53:47 +0500

    1. Схема прав пользователи и роли. [#1093]

---
+++ pgAdmin/users_roles/create.sql
@@ -0,0 +1,42 @@
+create table roles(
+	id serial primary key,
+	name varchar(255));
+	
+create table users(
+	id serial primary key,
+	name varchar(255), 
+	role_id int references roles(id));
+		
+create table rules(
+	id serial primary key,
+	name varchar(255));
+	
+create table role_ruls(
+	id serial primary key,
+	role_id int references roles(id), 
+	rule_id int references rules(id));
+
+create table state_item(
+	id serial primary key, 
+	status bool);
+	
+create table category_item(
+	id serial primary key, 
+	name varchar(255));
+
+create table items(
+	id serial primary key,
+	name varchar(255),
+	user_id references users(id)),
+	state_id references state_item(id),
+	category_id references category_item(id);
+
+create table comments_items(
+	id serial primary key,
+	comment text, 
+	item_id int references items(id));
+	
+create table attachs(
+	id serial primary key, 
+	name varchar(255), 
+	item_id int references items(id));
---
+++ pgAdmin/users_roles/insert.sql
@@ -0,0 +1,47 @@
+insert into roles(name) values ('напдающий');
+insert into roles(name) values ('защитник');
+insert into roles(name) values ('вратарь');
+
+insert into users(name, role_id) values ('Максим', 1);
+insert into users(name, role_id) values ('Роман', 2);
+insert into users(name, role_id) values ('Андрей', 3);
+
+insert into rules(name) values ('забивать голы');
+insert into rules(name) values ('остановить нападение');
+insert into rules(name) values ('отражать мяч');
+insert into rules(name) values ('защищать ворота');
+
+insert into role_ruls(role_id, rule_id) values (1, 1);
+insert into role_ruls(role_id, rule_id) values (2, 2);
+insert into role_ruls(role_id, rule_id) values (2, 4);
+insert into role_ruls(role_id, rule_id) values (3, 3);
+insert into role_ruls(role_id, rule_id) values (3, 4);
+
+insert into state_item(status) values (true);
+insert into state_item(status) values (false);
+
+insert into category_item(name) values ('очень важная');
+insert into category_item(name) values ('важная');
+insert into category_item(name) values ('средняя');
+
+insert into items(name, user_id, state_id, category_id) 
+            values ('забить гол в первом тайме', 1, 2, 3);
+insert into items(name, user_id, state_id, category_id) 
+            values ('забить победный гол', 1, 1, 1);
+insert into items(name, user_id, state_id, category_id) 
+            values ('не получить красную карточку', 2, 1, 2);
+insert into items(name, user_id, state_id, category_id) 
+            values ('не пропустить гол', 3, 1, 1);
+
+insert into comments_item(comment, item_id) 
+            values ('главное победить', 1);
+insert into comments_item(comment, item_id) 
+            values ('нужно больше бить по воротам', 2);
+insert into comments_item(comment, item_id) 
+            values ('меньше эмоций', 3);
+insert into comments_item(comment, item_id) 
+            values ('сохраняй бдительность', 4);
+
+insert into attachs(name, item_id) values ('схема игры', 2);
+insert into attachs(name, item_id) values ('схема игры', 3);
+insert into attachs(name, item_id) values ('схема игры', 4);
